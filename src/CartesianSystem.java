553ac93a1517b18281a81c3046607604d8e1f68d
Zagir Dingizbaev <zagir.dingizbaev@yandex.ru>
2018-12-07 13:39:45 +0300

    New form added

--- src/CartesianSystem.java
+++ src/CartesianSystem.java
@@ -7,25 +7,30 @@ import javax.imageio.ImageIO;
 
 public class CartesianSystem {
     public static void main(String[] args) throws IOException {
-        final int width = 800;
-        final int height = 800;
-        final int step = width/20;
-        BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        final int height = 880;
+        final int step = height / 22;
+        double leftBorder = -10;
+        double rightBorder = 10;
+        String s = "";
+        BufferedImage bufferedImage = new BufferedImage(height, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bufferedImage.createGraphics();
         g2d.setColor(Color.white);
-        g2d.fillRect(0, 0, width, height);
+        g2d.fillRect(0, 0, height, height);
         g2d.setColor(Color.black);
-        for (int i = 0; i < width; i += step) {
-            g2d.drawLine(i, 0, i, height);
+        for (int i = step; i <= height - step; i += step) {
+            g2d.drawLine(i, step, i, height - step);
         }
-        for (int i = 0; i < width; i += step) {
-            g2d.drawLine(0, i, width, i);
+        for (int i = step; i <= height - step; i += step) {
+            double countX = Math.abs(rightBorder - leftBorder) / 20.0 + i / step + leftBorder; //????
+            s = String.valueOf(countX);
+            g2d.drawLine(step, i, height - step, i);
+            g2d.drawString(s, i - 5, height - step + 12);
         }
         g2d.setStroke(new BasicStroke(2.0f));
-        g2d.drawLine(width / 2, 0, width / 2, height);
-        g2d.drawLine(0, height / 2, width, height / 2);
-        g2d.drawString("X", width - 10, height / 2 - 5);
-        g2d.drawString("Y", width / 2 + 5, 10);
+        g2d.drawLine(height / 2, step / 2, height / 2, height - step);
+        g2d.drawLine(step, height / 2, height - step / 2, height / 2);
+        g2d.drawString("X", height - 10, height / 2 - 5);
+        g2d.drawString("Y", height / 2 + 5, 10);
 //        int[] xArray = {}; Здесь будут параметры, которые мы будем получать из метода интерфейса
 //        int[] yArray = {};
 //        int nPoint = eps; необходимая нам точность в построении, (угловатость графика)
@@ -34,9 +39,9 @@ public class CartesianSystem {
 //        g.setColor(Color.black);
 //        g.drawString("y = f(x)",,); хз как подписать график (необходимо узнать позиуию постановки надписи)
         g2d.dispose();
-        File file = new File("D://Projects/FunctionsPainter/ResultImage/PngImage.png");
+        File file = new File("ResultImage/PngImage.png");
         ImageIO.write(bufferedImage, "png", file);
-        file = new File("D://Projects/FunctionsPainter/ResultImage/JpgImage.jpg");
+        file = new File("ResultImage/JpgImage.jpg");
         ImageIO.write(bufferedImage, "jpg", file);
     }
 }
