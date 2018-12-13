8900bdd5b001dbeb6c4d7c074bc07df3d991c2aa
Linar <zagidullinlinar24@gmail.com>
2018-12-13 20:29:16 +0300

    Вроде бы сделал веррный график на границах и разбил на отдельные функции

--- src/CartesianSystem.java
+++ src/CartesianSystem.java
@@ -7,64 +7,84 @@ import java.io.File;
 import java.io.IOException;
 
 public class CartesianSystem {
-    private final int SIZE = 880;
-    private final int POINTS = 100;
-
+    private int points;
+    private int size;
     private int numbersOfStep;
     private int step;
     private int leftBorder;
     private int rightBorder;
+    private int upBorder;
+    private int downBorder;
+    private BufferedImage bufferedImage;
+    private Graphics2D g2d;
 
     public CartesianSystem(int leftBorder, int rightBorder) {
         this.leftBorder = leftBorder;
         this.rightBorder = rightBorder;
-        this.numbersOfStep = rightBorder-leftBorder;
-        this.step = SIZE/(this.numbersOfStep +2);
-    }
+        if (Math.abs(leftBorder) > Math.abs(rightBorder)){
+            this.upBorder = Math.abs(leftBorder);
+            this.downBorder = (-1)*Math.abs(rightBorder);
+        }
+        else {
+            this.upBorder = Math.abs(rightBorder);
+            this.downBorder = (-1)*Math.abs(leftBorder);
+        }
+        this.size = 880;
+        this.points = (int)(size/2.5);
 
-    public void drawFunction(IFunction function) {
         String s = "";
-        BufferedImage bufferedImage = new BufferedImage(SIZE, SIZE, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2d = bufferedImage.createGraphics();
+        this.numbersOfStep = rightBorder-leftBorder;
+        this.step = size /(this.numbersOfStep +2);
+        this.bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+        this.g2d = bufferedImage.createGraphics();
         g2d.setFont(new Font("SansSerif", Font.BOLD, step /2));
         g2d.setColor(Color.white);
-        g2d.fillRect(0, 0, SIZE, SIZE);
+        g2d.fillRect(0, 0, size, size);
         g2d.setColor(Color.black);
-        g2d.setStroke(new BasicStroke(SIZE / 400));
-        for (int i = step; i <= SIZE - step; i += step) {
+        g2d.setStroke(new BasicStroke(size / 400));
+        for (int i = step; i <= size - step; i += step) {
             double countX = Math.abs(rightBorder - leftBorder) / 20.0 + i / step + leftBorder;
 //            g2d.drawString(s, 5, i + 8);
             double countY = Math.abs(rightBorder - leftBorder) / 20.0 + i / step + leftBorder;
-//            g2d.drawString(s, i - 5, SIZE - step + 12);
+//            g2d.drawString(s, i - 5, size - step + 12);
             s = String.valueOf(countX);
-            g2d.drawLine(step, i, SIZE - step, i);
-            g2d.drawLine(i, step, i, SIZE - step);
+            g2d.drawLine(step, i, size - step, i);
+            g2d.drawLine(i, step, i, size - step);
         }
-            g2d.drawString("X", SIZE - (int)(step *0.8), (this.leftBorder*(-1)+1)*step - step/4);
-            g2d.drawString("Y", (this.leftBorder*(-1)+1)*step + step/4, (int)(step *0.8));
-            if (this.leftBorder<0&&this.rightBorder>0){
-                g2d.setStroke(new BasicStroke(SIZE / 200));
-                g2d.drawLine((this.leftBorder*(-1)+1)*step, step / 2, (this.leftBorder*(-1)+1)*step, SIZE - step);
-                g2d.drawLine(step, (this.leftBorder*(-1)+1)*step, SIZE - step / 2, (this.leftBorder*(-1)+1)*step);
-            }
-            double[] xArray = new double[POINTS+1];
-            double[] yArray = new double[POINTS+1];
-            g2d.setColor(Color.red);
-            for (int j =0; j <= POINTS; j++ ) {
-                xArray [j] = this.leftBorder+j*(float) numbersOfStep /POINTS;
-                yArray [j] = function.calculate(xArray[j]);
-            }
-            for (int i = 0; i < POINTS; i++){
-                g2d.drawLine((int)(xArray[i]* step +(this.leftBorder*(-1)+1)*step),(int)((-1)*yArray[i]* step +(this.leftBorder*(-1)+1)*step),
-                        (int)(xArray[i+1]* step +(this.leftBorder*(-1)+1)*step),(int)((-1)*yArray[i+1]* step +(this.leftBorder*(-1)+1)*step));
-            }
-            File file = new File("ResultImage/image.bmp");
-            try {
-                ImageIO.write(bufferedImage,"bmp",file);
-            }
-            catch (IOException e){
-                System.out.println("ImageIO.write error"+e.getMessage());
+        g2d.drawString("X", size - (int)(step *0.8), (this.leftBorder*(-1)+1)*step - step/4);
+        g2d.drawString("Y", (this.leftBorder*(-1)+1)*step + step/4, (int)(step *0.8));
+        if (this.leftBorder<=0 && this.rightBorder>=0){
+            g2d.setStroke(new BasicStroke(size / 200));
+            g2d.drawLine((this.leftBorder*(-1)+1)*step, step / 2, (this.leftBorder*(-1)+1)*step, size - step);
+            g2d.drawLine(step, (this.leftBorder*(-1)+1)*step, size - step / 2, (this.leftBorder*(-1)+1)*step);
+        }
+    }
+
+    public void drawFunction(IFunction function) {
+        double[] xArray = new double[points + 1];
+        double[] yArray = new double[points + 1];
+        g2d.setColor(Color.red);
+        for (int j = 0; j <= points; j++) {
+            xArray[j] = this.leftBorder + j * (float) numbersOfStep / points;
+            yArray[j] = function.calculate(xArray[j]);
+        }
+        for (int i = 0; i < points; i++) {
+            if (Double.isNaN(yArray[i + 1]) || Double.isNaN(yArray[i]) || yArray[i] > upBorder || yArray[i+1] > upBorder
+                    || yArray[i] < downBorder || yArray[i + 1] < downBorder) {
+                continue;
+            } else {
+                g2d.drawLine((int) (xArray[i] * step + (this.leftBorder * (-1) + 1) * step), (int) ((-1) * yArray[i] * step + (this.leftBorder * (-1) + 1) * step),
+                        (int) (xArray[i + 1] * step + (this.leftBorder * (-1) + 1) * step), (int) ((-1) * yArray[i + 1] * step + (this.leftBorder * (-1) + 1) * step));
             }
         }
+    }
+    public void safe(String path,String format){
+        File file = new File(path);
+        try {
+            ImageIO.write(bufferedImage,format,file);
+        }
+        catch (IOException e){
+            System.out.println("ImageIO.write error"+e.getMessage());}
+            }
     }
 
