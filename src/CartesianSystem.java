38d47f939cdf740a824957ac3bc8fb5daaaf2d9b
Linar <zagidullinlinar24@gmail.com>
2018-12-14 14:44:05 +0300

    commite на паре

--- src/CartesianSystem.java
+++ src/CartesianSystem.java
@@ -18,51 +18,57 @@ public class CartesianSystem {
     private BufferedImage bufferedImage;
     private Graphics2D g2d;
 
-    public CartesianSystem(int leftBorder, int sizeOfCube,int size) {
+    public CartesianSystem(int leftBorder, int sizeOfCube, int size) {
         this.leftBorder = leftBorder;
-        this.rightBorder = leftBorder+sizeOfCube;
-        this.upBorder = sizeOfCube/2;
-        this.downBorder = (-1)*upBorder;
+        this.rightBorder = leftBorder + sizeOfCube;
+        this.upBorder = sizeOfCube / 2;
+        this.downBorder = (-1) * upBorder;
         this.size = size;
-        this.points = (int)(size/2);
-        this.numbersOfStep = sizeOfCube+2;
+        this.points = (int) (size / 2);
+        this.numbersOfStep = sizeOfCube + 2;
 
-        String s = "";
-        this.step = size /(this.numbersOfStep);
-        this.bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
-        this.g2d = bufferedImage.createGraphics();
-        g2d.setFont(new Font("SansSerif", Font.BOLD, step /2));
+        String sx = "";
+        String sy = "";
+        step = size / (numbersOfStep);
+        bufferedImage = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+        g2d = bufferedImage.createGraphics();
+        g2d.setFont(new Font("SansSerif", Font.BOLD, step / 2));
         g2d.setColor(Color.white);
         g2d.fillRect(0, 0, size, size);
         g2d.setColor(Color.black);
         g2d.setStroke(new BasicStroke(size / 400));
+        int countX = leftBorder;
+        int countY = upBorder;
         for (int i = step; i <= size - step; i += step) {
-            double countX = Math.abs(rightBorder - leftBorder) / 20.0 + i / step + leftBorder;
-//            g2d.drawString(s, 5, i + 8);
-            double countY = Math.abs(rightBorder - leftBorder) / 20.0 + i / step + leftBorder;
-//            g2d.drawString(s, i - 5, size - step + 12);
-            s = String.valueOf(countX);
+            sx = String.valueOf(countX);
+            sy = String.valueOf(countY);
+            g2d.drawString(sx, i - 40, size - step + 12);
+            countX ++;
+            g2d.drawString(sy, 15, i + 8);
+            countY --;
             g2d.drawLine(step, i, size - step, i);
             g2d.drawLine(i, step, i, size - step);
         }
-        g2d.drawString("X", size - (int)(step *0.8), (upBorder + 1)*step - step/4);
-        g2d.drawString("Y", (this.leftBorder*(-1)+1)*step + step/4, (int)(step *0.8));
-        if (this.leftBorder<=0 && this.rightBorder>=0 && upBorder>0 && downBorder<0){
+
+        g2d.drawString("X", size - (int) (step * 0.8), (upBorder + 1) * step - step / 2);
+        g2d.drawString("Y", (leftBorder * (-1) + 1) * step + step / 4, (int) (step * 0.8));
+        if (leftBorder <= 0 && rightBorder >= 0 && upBorder > 0 && downBorder < 0) {
             g2d.setStroke(new BasicStroke(size / 200));
-            g2d.drawLine((this.leftBorder*(-1)+1)*step, step / 2, (this.leftBorder*(-1)+1)*step, size - step);
-            g2d.drawLine(step, (upBorder + 1)*step, size - step / 2, (upBorder + 1)*step);
+            g2d.drawLine((leftBorder * (-1) + 1) * step, step / 2, (leftBorder * (-1) + 1) * step, size - step);
+            g2d.drawLine(step, (upBorder + 1) * step, size - step / 2, (upBorder + 1) * step);
+            g2d.drawLine(size - step / 2, (upBorder + 1) * step, size - step * 3 / 4, (int) ((upBorder + 1 - 0.3) * step));
         }
     }
 
-    public void drawFunction(IFunction function,Color color) {
+    public void drawFunction(IFunction function, Color color) {
         double[] xArray = new double[points + 1];
         double[] yArray = new double[points + 1];
         g2d.setColor(color);
         for (int j = 0; j <= points; j++) {
             xArray[j] = this.leftBorder + j * (float) numbersOfStep / points;
             yArray[j] = function.calculate(xArray[j]);
-            if (yArray[j] > upBorder || yArray[j] < downBorder){
-                yArray[j] = 0.0/0.0;
+            if (yArray[j] > upBorder || yArray[j] < downBorder) {
+                yArray[j] = 0.0 / 0.0;
             }
         }
         for (int i = 0; i < points; i++) {
@@ -77,13 +83,14 @@ public class CartesianSystem {
             }
         }
     }
-    public void safe(String path,String format){
+
+    public void safe(String path, String format) {
         File file = new File(path);
         try {
-            ImageIO.write(bufferedImage,format,file);
+            ImageIO.write(bufferedImage, format, file);
+        } catch (IOException e) {
+            System.out.println("ImageIO.write error" + e.getMessage());
         }
-        catch (IOException e){
-            System.out.println("ImageIO.write error"+e.getMessage());}
-            }
     }
+}
 
